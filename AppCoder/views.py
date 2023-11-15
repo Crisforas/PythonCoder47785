0ff7bac72fa92074f333551bd7f55d02ec8f25eb
CrisForas <cristianf87@gmail.com>
2023-11-15 20:28:52 -0300

    first commit

---
+++ AppCoder/views.py
@@ -0,0 +1,18 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from AppCoder.models import Curso
+
+
+# Create your views here.
+def crear_curso(request):
+    curso = Curso(nombre="Python", camada=47785)
+    curso.save()
+    contexto = {"curso": curso}
+
+    return render(request, 'index.html', contexto)
+
+
+def show_html(request):
+    curso = Curso.objects.first()
+    contexto = {"curso": curso, "nombre": "Luis"}
+    return render(request, 'index.html', contexto)
